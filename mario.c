cdb56cde2fd257ae4e36337f8fdd956114d46d0d
Kl0ak3d <Kl0ak3d@users.noreply.github.com>
2024-04-21 14:25:49 -0400

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -2,27 +2,37 @@
 #include <cs50.h>
 
 int main(void) {
-    int height = get_int("Height: ");
-
-    // Loop over rows
-    for (int i = 0; i < height; i++) {
-        // Print spaces for left pyramid
-        for (int j = 0; j < height - i - 1; j++) {
-            printf(" ");
+    int height;
+    do {
+        height = get_int("Height: ");
+        if (height == -1) {
+            printf("Please enter a height greater than or equal to 0.\n");
         }
-        // Print hashes for left pyramid
-        for (int j = 0; j <= i; j++) {
-            printf("#");
+    } while (height <= -1);
+
+    if (height == 2) {
+        printf("height: %d\n", height);
+    } else {
+        // Loop over rows
+        for (int i = 0; i < height; i++) {
+            // Spaces for left pyramid
+            for (int j = 0; j < height - i - 1; j++) {
+                printf(" ");
+            }
+            //Left pyramid
+            for (int j = 0; j <= i; j++) {
+                printf("#");
+            }
+
+            //Gap between pyramids
+            printf("  ");
+
+            //Right pyramid
+            for (int j = 0; j <= i; j++) {
+                printf("#");
+            }
+
+            printf("\n");
         }
-
-        // Print gap between pyramids
-        printf("  ");
-
-        // Print hashes for right pyramid
-        for (int j = 0; j <= i; j++) {
-            printf("#");
-        }
-
-        printf("\n");
     }
 }
