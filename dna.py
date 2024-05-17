8d54f33afad038bf0913e6f0e36b5e46be1deac0
Kl0ak3d <Kl0ak3d@users.noreply.github.com>
2024-05-17 11:27:42 -0400

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -2,71 +2,42 @@ from sys import argv, exit
 import csv
 
 
-def main():
-    if len(argv) != 3:
-        print("Usage: python dna.py data.csv sequence.txt")
-        exit(1)
+# Checks Usage
+if len(sys.argv) != 3:
+    sys.exit("Usage: python dna.py data.csv sequence.txt")
 
-    STRs = []
-    profiles = []
-
-    with open(argv[1], mode="r") as database:
-        reader = csv.DictReader(database)
-
-        STRs = reader.fieldnames[1:]
-        for row in reader:
-
-            profiles.append(row)
-
-    seq_str_count = dict.fromkeys(STRs, 0)
-
-    with open(argv[2], mode="r") as sequence_file:
-
-        sequence = sequence_file.readline()
-
-        for STR in STRs:
-
-            seq_str_count[STR] = find_repeats(sequence, STR)
-
-    for profile in profiles:
-        match_count = 0
-
-        for STR in STRs:
-            if int(profile[STR]) != seq_str_count[STR]:
-                continue
-            match_count += 1
-
-        if match_count == len(STRs):
-            print(profile['name'])
-            exit(0)
-
-    print("No match")
-    exit(1)
+# Open & Read CSV
+with open(argv[1], "r") as inputfile:
+    reader = list(csv.reader(inputfile))
+    reader[0].remove("name")
+    i = reader[0]
+# Open TxT
+ with open(seq_path, "r") as file:
+        sequence = file.read()
+#Compute Repeats
+  max_counts = []    for i in range(1, len(reader.fieldnames)):
+        STR = reader.fieldnames[i]
+        max_counts.append(0)
+#Loop
+ if line[strepeat] == str(sequenceRepeats[strepeat]):
+        strsMatched += 1
+        if strsMatched == len(checkSTRS):
+              matchedPerson = line['name']
+              break
+        print(matchedPerson)
+#Function
+ def compute(pattern, dnaSeq):
+       if match == []:
+        myresult = 0
+        return myresult
+#Compares data
+   for i in range(len(dict_list)):
+        matches = 0
+        for j in range(1, len(reader.fieldnames)):
+              if int(max_counts[j - 1]) == int(dict_list[i]  [reader.fieldnames[j]]):
+                matches += 1
+            if matches == (len(reader.fieldnames) - 1):
+                print(dict_list[i]['name'])
+                exit(0)print("No match")
 
 
-def find_repeats(sequence, STR):
-
-    L = len(STR)
-
-    max_repeats = 0
-    for i in range(len(sequence)):
-
-        repeats = 0
-
-        if sequence[i: i + L] == STR:
-
-            repeats += 1
-
-            while sequence[i: i + L] == sequence[i + L: i + (2 * L)]:
-                repeats += 1
-
-                i += L
-
-        if repeats > max_repeats:
-            max_repeats = repeats
-
-    return max_repeats
-
-
-if __name__ == "__main__":
-    main()
