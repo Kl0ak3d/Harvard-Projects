5dacac0fa29ff4fd2d61c8f83788ae50c0cf2f42
Kl0ak3d <Kl0ak3d@users.noreply.github.com>
2024-05-17 12:10:34 -0400

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -1,45 +1,61 @@
 from cs50 import get_int
 
 def main():
-
-    card = get_int("Number: ")
-
-    if valid_card(card):
-        card_company(card)
-
-    else:
-        print("INVALID")
-
-
-def valid_card(num):
-
-    sum = 0
-    for i, c in enumerate(reversed(str(num))):
-        if i % 2 == 0:
-            sum += int(c)
+    while True:
+        number = get_int("Number: ")
+        if number < 0:
+            print("The number must be a non-negative integer!")
         else:
-            for j in str(int(c) * 2):
-                sum += int(j)
+            break
 
-    if sum % 10 == 0:
-        return True
-    else:
-        return False
+    digits, sum_of_odds, sum_of_evens, last, sec_last = count_text_stats(number)
+    first_two_digits = last * 10 + sec_last
+    total_sum = sum_of_odds + sum_of_evens
 
-
-def card_company(card):
-    num = int(str(card)[0:2])
-
-    if (num is 34 or num is 37) and len(str(card)) is 15:
-        print("AMEX")
-    elif num > 50 and num < 56 and len(str(card)) is 16:
-        print("MASTERCARD")
-    elif num >= 40 and num < 50 and (len(str(card)) is 13 or len(str(card)) is
-                                     16):
-        print("VISA")
+    if is_valid(total_sum):
+        if digits == 13 or digits == 16 and is_visa(last):
+            print("VISA")
+        elif digits == 15 and is_amex(first_two_digits):
+            print("AMEX")
+        elif digits == 16 and is_mastercard(first_two_digits):
+            print("MASTERCARD")
+        else:
+            print("INVALID")
     else:
         print("INVALID")
 
+def count_text_stats(number):
+    digits = 0
+    sum_of_odds = 0
+    sum_of_evens = 0
+    last = sec_last = 0
+
+    while number > 0:
+        digits += 1
+        sec_last = last
+        last = number % 10
+
+        if digits % 2 == 0:
+            multiplier = last * 2
+            sum_of_evens += multiplier // 10 + multiplier % 10 if multiplier >= 10 else multiplier
+        else:
+            sum_of_odds += last
+
+        number //= 10
+
+    return digits, sum_of_odds, sum_of_evens, last, sec_last
+
+def is_visa(first_digit):
+    return first_digit == 4
+
+def is_amex(first_two_digits):
+    return first_two_digits == 34 or first_two_digits == 37
+
+def is_mastercard(first_two_digits):
+    return 51 <= first_two_digits <= 55
+
+def is_valid(total_sum):
+    return total_sum % 10 == 0
 
 if __name__ == "__main__":
     main()
