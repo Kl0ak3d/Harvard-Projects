da0c59cc1476360f5dbcbbdda64421113f359579
Kl0ak3d <Kl0ak3d@users.noreply.github.com>
2024-05-17 11:40:35 -0400

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -10,6 +10,38 @@ def main():
 
     db = []
     sequence = ""
+    def find_max_strs(dna_sequence, str_patterns):
+    max_counts = {str_pattern: 0 for str_pattern in str_patterns}
+
+    for str_pattern in str_patterns:
+        pattern_length = len(str_pattern)
+        max_repeat_count = 0
+
+        for i in range(len(dna_sequence)):
+            repeat_count = 0
+            j = i
+
+            # Count repeats of the current STR pattern starting from position i
+            while dna_sequence[j:j + pattern_length] == str_pattern:
+                repeat_count += 1
+                j += pattern_length
+
+            # Update max repeat count if necessary
+            if repeat_count > max_repeat_count:
+                max_repeat_count = repeat_count
+
+        # Update the maximum count for this STR pattern
+        max_counts[str_pattern] = max_repeat_count
+
+ AGATC = compute('AGATC', text)
+    AATG = compute('AATG', text)
+    TATC = compute('TATC', text)
+    TTTTTTCT = compute('TTTTTTCT', text)
+    TCTAG = compute('TCTAG', text)
+    GATA = compute('GATA', text)
+    GAAA = compute('GAAA', text)
+    TCTG = compute('TCTG', text)
+
 
     # Open & Read CSV
     with open(db_path, "r") as csvfile:
